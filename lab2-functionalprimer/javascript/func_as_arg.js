c384426795380b8f0e2752ec26651b82671040cb
Hafsa Muse <220034513@aston.ac.uk>
2025-02-19 21:20:21 +0000

    completed the final task for lab 2

--- lab2-functionalprimer/javascript/func_as_arg.js
+++ lab2-functionalprimer/javascript/func_as_arg.js
@@ -1,24 +1,20 @@
-//Create list of ints from 1 to 5, Haskell equivalent [1..5]
-function arrFunc(){
+// Create a function to generate a list of numbers from a to b
+function arrFunc(a, b) {
     let arr = [];
-    for (let i = 1; i<=5; i++) {
+    for (let i = a; i <= b; i++) {
         arr.push(i);
     }
-    return arr;    
+    return arr;
 }
 
-function applicatorFunc(inpFunc, s){
-    if(s=='s'){
-        const arr = inpFunc();        
-        let sum = arr.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
-        return sum;
-    }
-    else{        
-        const arr = inpFunc();
-        let sum = arr.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
-        return sum/5;
-    }
+// Define applicatorFunc to compute sum or average
+function applicatorFunc(inpFunc, a, b, s) {
+    const arr = inpFunc(a, b);
+    let sum = arr.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+
+    return s === 's' ? sum : sum / arr.length;
 }
 
-let x = applicatorFunc(arrFunc, 's');
-console.log(x);
+// Call applicatorFunc with range 1 to 10 and choice 's' (sum) or 'a' (average)
+console.log(applicatorFunc(arrFunc, 1, 10, 's')); // Sum
+console.log(applicatorFunc(arrFunc, 1, 10, 'a')); // Average
